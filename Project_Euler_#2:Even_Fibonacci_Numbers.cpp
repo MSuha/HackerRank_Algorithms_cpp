c2fef9f9cf7d2a108d5685d4682d6d17fcd78dd5
muhammedsuhademirel <msuhademirel@gmail.com>
2019-09-06 14:29:20 +0300

    Project Euler #1: Multiples of 3 and 5 -> O(n.m) I guess

---
+++ Project_Euler_#2:Even_Fibonacci_Numbers.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long fib(long long a){
+    long long n = 1, m = 2, cnt, tmp;
+
+    for (cnt = 0; m < a;){
+        if(m%2 ==0)
+            cnt  += m;
+        tmp = m;
+        m = n + m;
+        n = tmp;
+    }
+    return cnt;
+}
+
+int main() {
+    int test_size;
+    long long input;
+
+    cin >> test_size;
+
+    for(int i = 0; i < test_size; i++){
+        cin >> input;
+        cout << fib(input) << endl;
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
