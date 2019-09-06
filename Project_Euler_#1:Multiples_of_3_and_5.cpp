6ec89b464d5e75a06be3e33cad1e233ffa69e20e
muhammedsuhademirel <msuhademirel@gmail.com>
2019-09-06 14:14:35 +0300

    Project Euler #1: Multiples of 3 and 5 -> O(n)

--- Project_Euler_#1:Multiples_of_3_and_5.cpp
+++ Project_Euler_#1:Multiples_of_3_and_5.cpp
@@ -5,8 +5,26 @@
 #include <algorithm>
 using namespace std;
 
+long long sum(long long a){
+    return ( a * (a+1) / 2);
+}
 
 int main() {
+    long long test_case, input;
+
+    cin >> test_case;
+
+    for(int i = 0; i < test_case; i++){
+        cin >> input;
+        input -= 1;
+
+        long long cnt = 0;
+        cnt += 3 * sum(input/3);
+        cnt += 5 * sum(input/5);
+        cnt -= 15 * sum(input/15);
+
+        cout << cnt << endl;
+    }
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
 }
